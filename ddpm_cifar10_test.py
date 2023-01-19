2034f0a013f2d8081ec62bc48bb07b9b6dfdff2b
students <kakorolev@beleriand.velkerr.ru>
2023-01-19 23:18:48 +0000

    Sampling by batches

--- ddpm_cifar10_test.py
+++ ddpm_cifar10_test.py
@@ -9,12 +9,18 @@ import os
 
 from model.datasets import get_CIFAR10
 from model.ddpm import DDPM
-from model.unet import UNet3
+from model.v2.unet import Unet
 from model.training import train, sample
-from model.utils import SaveBestModel, load_model
+from model.utils import SaveBestModel, load_model, plot_images
+from model.metrics import fid_score
+
+import gc
+
 
 def main(args):
     batch_size = args.batch_size
+
+    print(f'Batch size {batch_size}')
 
     print('Loading CIFAR10...')
     dataloader = get_CIFAR10(batch_size=batch_size)
@@ -23,7 +29,9 @@ def main(args):
     print(f'Using device {device}')
 
     n_epochs = args.epochs
-    model = DDPM(UNet3().to(device), device=device)
+    
+    unet = Unet(T=1000, n_channels=128, channels_mul=[1, 2, 2, 2], n_residual_blocks=2, dropout=0.1).to(device)
+    model = DDPM(unet, device=device)
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
     #optimizer = optim.SGD(model.parameters(), lr=args.lr)
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer)
@@ -31,6 +39,10 @@ def main(args):
 
     model_saver = SaveBestModel()
     start_epoch = 0
+
+    # Empty cache
+    torch.cuda.empty_cache()
+    print("Collected: {}".format(gc.collect()))
 
     if os.path.exists(args.path):
         loss = float("inf")
@@ -43,18 +55,39 @@ def main(args):
 
     if args.command == 'train':
         print('Training the model...')
-        loss_log = train(model, dataloader, optimizer, criterion, device, scheduler=None, n_epochs=n_epochs, start_epoch=start_epoch, model_path=args.path, model_saver=model_saver)
+        loss_log = train(model, dataloader, optimizer, criterion, device, scheduler=None, n_epochs=start_epoch + n_epochs, start_epoch=start_epoch, model_path=args.path, model_saver=model_saver)
     elif args.command == 'sample':
-        print('Sampling...')
-        img_shape = tuple(next(iter(dataloader))[0].shape[1:])
-        sample(model, args.samples, img_shape, device, step=args.step, output_dir=args.output)
+        num_samples = max(args.samples, batch_size)
+        print(f'Sampling {num_samples} images...')
+        
+        img_shape = dataloader.dataset[0][0].shape
+        sampled_images = sample(model, num_samples, img_shape, args.batch_size, device, step=args.step)
+        
+        if not os.path.exists(args.output):
+            os.mkdir(args.output)
+
+        plot_images(sampled_images, "Sampled images", output_filename=os.path.join(args.output, f"sampled.png"))
+
+    elif args.command == 'metrics':
+        print('Calculating metrics...')
+        real_data = []
+
+        for images, _ in dataloader:
+            real_data.append(images)
+        
+        print('Real data is collected')
+        real_data = torch.cat(real_data, dim=0)
+        fake_data = real_data #sample(models, )
+        fid = fid_score(real_data, fake_data)
+
+        print('FID {:.5f}'.format(fid))
     else:
         print(f'Unknown command: {args.command}')
 
 
 if __name__ == "__main__":
     parser = ArgumentParser(description='DDPM on CIFAR10')
-    parser.add_argument('command', help='train or sample')
+    parser.add_argument('command', help='train,sample,metrics')
     parser.add_argument('-b', '--batch-size', type=int, help='Batch size', default=128)
     parser.add_argument('-e', '--epochs', type=int, help='Number of epochs', default=20)
     parser.add_argument('-lr', type=float, help='Adam learning rate', default=2e-4)
--- model/training.py
+++ model/training.py
@@ -2,40 +2,47 @@ import torch
 from tqdm import tqdm
 import os
 
-from model.utils import plot_images
 
-def sample(model, n_samples, img_shape, device, step, output_dir):
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
+def sample(model, n_samples, img_shape, batch_size, device, step):
+    model.eval()
+    slicer = range(0, n_samples, batch_size)
+    sampled_images = []
 
     with torch.no_grad():
-        x = torch.randn(n_samples, *img_shape).to(device)
-        plot_images(x, f"t={model.T}", output_filename=os.path.join(output_dir, f"sampled_{model.T}.png"))
+        for i in slicer:
+            x = torch.randn(n_samples, *img_shape).to(device)
 
-        for t in tqdm(range(model.T - 1, -1, -1)):
-            t_batch = (torch.ones(n_samples, 1) * t).to(device).long()
-            eps_batch = model.backward(x, t_batch)
+            #if output_dir is not None:
+            #    plot_images(x, f"t={model.T}", output_filename=os.path.join(output_dir, f"sampled_{model.T}.png"))
 
-            alpha_t = model.alphas[t]
-            alpha_prod = model.alpha_prods[t]
+            for t in tqdm(range(model.T - 1, -1, -1)):
+                t_batch = (torch.ones(n_samples) * t).to(device).long()
+                eps_batch = model.backward(x, t_batch)
 
-            x = (1 / alpha_t.sqrt()) * (x - (1 - alpha_t) / (1 - alpha_prod).sqrt() * eps_batch)
+                alpha_t = model.alphas[t]
+                alpha_prod = model.alpha_prods[t]
 
-            if t > 0:
-                noise = torch.randn(n_samples, *img_shape).to(device)
+                x = (1 / alpha_t.sqrt()) * (x - (1 - alpha_t) / (1 - alpha_prod).sqrt() * eps_batch)
 
-                # option 1 from the article for sigma_t
-                # both versions from the article give the same result
-                sigma_t = model.betas[t].sqrt()
+                if t > 0:
+                    noise = torch.randn(n_samples, *img_shape).to(device)
 
-                x = x + sigma_t * noise
+                    # option 1 from the article for sigma_t
+                    # both versions from the article give the same result
+                    sigma_t = model.betas[t].sqrt()
 
-            if t % step == 0:
-                plot_images(x, f"t={t}", output_filename=os.path.join(output_dir, f"sampled_{t}.png"))
+                    x = x + sigma_t * noise
 
+                #if t % step == 0 and output_dir is not None:
+                #    plot_images(x, f"t={t}", output_filename=os.path.join(output_dir, f"sampled_{t}.png"))
+            sampled_images.append(x)
+    
+    sampled_images = torch.cat(sampled_images, dim=0)
+    return sampled_images
 
 def train(model, dataloader, optimizer, criterion, device, scheduler, n_epochs, start_epoch, model_saver, model_path):
     loss_log = []
+    model.train()
 
     for epoch in range(start_epoch, n_epochs):
         epoch_loss = 0.0
