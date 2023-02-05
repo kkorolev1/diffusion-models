381fd92fc9b89e53ed5461b970ecbdb1835fb508
Kirill Korolev <kafafyf@gmail.com>
2023-02-05 09:50:25 +0000

    Simplifying sampling

--- model/training.py
+++ model/training.py
@@ -5,41 +5,16 @@ from tqdm import tqdm
 @torch.no_grad()
 def sample(model, n_samples, img_shape, batch_size, device):
     model.eval()
-    slicer = range(0, n_samples, batch_size)
     sampled_images = []
 
     print(f'Going to sample {n_samples // batch_size} batches')
     
-    for i in slicer:
+    for i in range(0, n_samples, batch_size):
         print(f'Sampling #{i // batch_size + 1} batch...')
-
         size = min(batch_size, n_samples - i)
-        x = torch.randn(size, *img_shape).to(device)
-
-        #if output_dir is not None:
-        #    plot_images(x, f"t={model.T}", output_filename=os.path.join(output_dir, f"sampled_{model.T}.png"))
-
-        for t in tqdm(range(model.T - 1, -1, -1)):
-            t_batch = torch.full((size,), t).to(device).long()
-            eps_batch = model.backward(x, t_batch)
-
-            alpha_t = model.alphas[t]
-            alpha_prod = model.alpha_prods[t]
-
-            x = (1 / alpha_t.sqrt()) * (x - (1 - alpha_t) / (1 - alpha_prod).sqrt() * eps_batch)
-
-            if t > 0:
-                noise = torch.randn_like(x).to(device)
-
-                # option 1 from the article for sigma_t
-                # both versions from the article give the same result
-                sigma_t = model.betas[t].sqrt()
-
-                x = x + sigma_t * noise
-
-            #if t % step == 0 and output_dir is not None:
-            #    plot_images(x, f"t={t}", output_filename=os.path.join(output_dir, f"sampled_{t}.png"))
-        sampled_images.append(x)
+        x_T = torch.randn(size, *img_shape).to(device)
+        x_0 = model(X_t).cpu()
+        sampled_images.append(x_0)
     
     sampled_images = torch.cat(sampled_images, dim=0)
     return sampled_images
